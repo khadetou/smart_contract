69699c951ceb4bc78542011332e3a7642f980526
khadetou <khadetou96@gmail.com>
2022-01-01 12:45:53 +0000

    transaction config

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,14 @@
 import hre from "hardhat";
 
 const main = async (): Promise<void> => {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
   const Transactions = await hre.ethers.getContractFactory("Transactions");
   const transactions = await Transactions.deploy();
-
-  await transactions.deployed();
 
   console.log("Transaction deployed to:", transactions.address);
 };
