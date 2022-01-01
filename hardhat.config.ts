1db52777f2b88ea7856af0ea0577e4b0db4d4a0e
khadetou <khadetou96@gmail.com>
2022-01-01 10:44:41 +0000

    hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,14 @@
-import { task } from "hardhat/config";
+//https://eth-ropsten.alchemyapi.io/v2/Z-z_om0mw4k0E8sl3-8kqt_fGW2LB4rs
 import "@nomiclabs/hardhat-waffle";
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 export default {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/Z-z_om0mw4k0E8sl3-8kqt_fGW2LB4rs",
+      accounts: [
+        "180820e12ccf3e55d81d794fdc15141145daef56caa09661e3279bf41b5f1e25",
+      ],
+    },
+  },
 };
